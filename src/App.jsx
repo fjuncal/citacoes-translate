3e74a18855822fd5aee0abcb2371cb76eb152112
Fellipe Juncal <fjuncal7@gmail.com>
2024-05-16 18:12:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h4>TESTE BOOTSTRAP</h4>
+    </>
+  );
+}
+
+export default App;
